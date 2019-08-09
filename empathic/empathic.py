99404830e79ca3673f490498d4d5b6400271bb23
Christian Blume <chr.blume@gmail.com>
2019-08-09 23:59:01 +1200

    fix stuff

--- empathic/empathic.py
+++ empathic/empathic.py
@@ -6,20 +6,73 @@ import numpy as np
 import json
 import uuid
 import sys
+import tempfile
+import os
+from contextlib import contextmanager
+import pickle
 
 app = Flask(__name__)
 
-heatmap = np.array([[0] * 100] * 160, dtype=float)
+tmpdir = tempfile.gettempdir()
+lockfile = os.path.join(tmpdir, 'empathic.lock')
+datafile = os.path.join(tmpdir, 'empathic.dat')
+
+
+def acquire_lock():
+    while True:
+        try:
+            with open(lockfile, "x") as _:
+                break
+        except FileExistsError:
+            pass
+
+        
+def release_lock():
+    try:
+        os.unlink(lockfile)
+    except:
+        pass
+
+
+@contextmanager
+def lock(write=False):
+    acquire_lock()
+    data = read_data()
+    try:
+        yield data
+    finally:
+        if write:
+            write_data(data)
+        release_lock()
+        
+
+class Data:
+    def __init__(self):
+        self.heatmap = np.array([[0] * 100] * 160, dtype=float)
+        self.states = {}  # session -> state
+
 
 class State:
     def __init__(self, ix, iy):
         self.ix = ix
         self.iy = iy
 
-states = {}  # session -> state
 
-sessions = []
+def read_data():
+    try:
+        with open(datafile, 'rb') as f:
+            return pickle.load(f)
+    except:
+        return Data()
 
+
+def write_data(data):
+    try:
+        with open(datafile, 'wb') as f:
+            pickle.dump(data, f)
+    except:
+        pass
+    
 
 def get_coordinate(value):
     try:
@@ -31,52 +84,29 @@ def get_coordinate(value):
     return value
 
 
-def check_session(s):
-    if sessions.count(s) == 0:
-        abort(status.HTTP_400_BAD_REQUEST)
-
-
-# TODO: Implement real login
-@app.route('/login', methods=["POST"])
-def login():
-    s = str(uuid.uuid1())
-    sessions.append(s)
-    return s, status.HTTP_200_OK
-
-
-# TODO: Implement real logout
-@app.route('/logout', methods=["POST"])
-def logout():
-    s = request.form.get("s")
-    if s is not None:
-        sessions.remove(s)
-    return '', status.HTTP_200_OK
-
-
 @app.route('/press', methods=["POST"])
-def action():
+def press():
     s = request.form.get("s")
-    check_session(s)
-
     x = request.form.get("x")
     y = request.form.get("y")
-    if x is None or y is None:
+    if x is None or y is None or s is None:
         abort(status.HTTP_400_BAD_REQUEST)
 
     x = get_coordinate(x)
     y = get_coordinate(y)
 
-    ix = int(heatmap.shape[0] * x)
-    iy = int(heatmap.shape[1] * y)
+    with lock(True) as data:
+        ix = int(data.heatmap.shape[0] * x)
+        iy = int(data.heatmap.shape[1] * y)
 
-    st = states.get(s)
+        st = data.states.get(s)
 
-    if st is not None:
-        heatmap[st.ix, st.iy] -= 1
+        if st is not None:
+            data.heatmap[st.ix, st.iy] -= 1
 
-    heatmap[ix, iy] += 1
+        data.heatmap[ix, iy] += 1
         
-    states[s] = State(ix, iy)
+        data.states[s] = State(ix, iy)
 
     return '', status.HTTP_200_OK
 
@@ -84,28 +114,31 @@ def action():
 @app.route('/release', methods=["POST"])
 def release():
     s = request.form.get("s")
-    check_session(s)
+    if s is None:
+        abort(status.HTTP_400_BAD_REQUEST)
 
-    st = states.get(s)
+    with lock(True) as data:
+        st = data.states.get(s)
 
-    if st is not None:
-        heatmap[st.ix, st.iy] -= 1
-        del states[s]
+        if st is not None:
+            data.heatmap[st.ix, st.iy] -= 1
+            del data.states[s]
 
     return '', status.HTTP_200_OK
 
 
 @app.route('/heat', methods=["POST"])
 def heat():
-    s = request.form.get("s")
-    check_session(s)
+    with lock() as data:
+        heatmap = np.copy(data.heatmap)
 
     heatsum = np.sum(np.sum(heatmap))
     if heatsum > 0:
         heatnorm = heatmap / heatsum
     else:
         heatnorm = heatmap
-    data = []
+
+    rows = []
     for i in range(heatnorm.shape[0]):
         for j in range(heatnorm.shape[1]):
             value = heatnorm[i, j]
@@ -113,13 +146,22 @@ def heat():
                 row = "{},{},{}".format(round(i / heatnorm.shape[0], 2),
                                         round(j / heatnorm.shape[1], 2),
                                         round(value, 2))
-                data.append(row)
-    return json.dumps(data)
+                rows.append(row)
+
+    return json.dumps(rows), status.HTTP_200_OK
 
 
 @app.route('/', methods=["GET"])
 def root():
     return "This is Empathic.", status.HTTP_200_OK
+
+
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "max-age=0, no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    return r
 
 
 def main():
